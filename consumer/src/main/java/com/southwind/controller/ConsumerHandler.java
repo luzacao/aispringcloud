e482bf3b7046d6b484525a942fcc8b6e5f174c80
luzacao <1565081157@qq.com>
2020-07-31 16:54:26 +0800

    Initial commit

---
+++ consumer/src/main/java/com/southwind/controller/ConsumerHandler.java
@@ -0,0 +1,55 @@
+package com.southwind.controller;
+
+import com.southwind.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerHandler {
+    @Autowired
+    private RestTemplate restTemplate;
+    @GetMapping("/findAll")
+    public Collection<Student> findAll(){
+        return restTemplate.getForEntity("http://localhost:8010/student/findAll",Collection.class).getBody();
+    }
+
+    @GetMapping("/findAll2")
+    public Collection<Student> findAll2(){
+        return restTemplate.getForObject("http://localhost:8010/student/findAll",Collection.class);
+    }
+
+    @GetMapping("/findById/{id}")
+    public Collection<Student> findById(@PathVariable("id") long id){
+        return restTemplate.getForEntity("http://localhost:8010/student/findAll",Collection.class).getBody();
+    }
+
+    @GetMapping("/findById2/{id}")
+    public Collection<Student> findById2(@PathVariable("id") long id){
+        return restTemplate.getForObject("http://localhost:8010/student/findAll",Collection.class);
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody Student student){
+        restTemplate.postForEntity("http://localhost:8010/student/save",student,null).getBody();
+    }
+
+    @PostMapping("/save2")
+    public void save2(@RequestBody Student student){
+        restTemplate.postForObject("http://localhost:8010/student/save",student,null);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody Student student){
+        restTemplate.put("http://localhost:8010/student/save",student);
+    }
+
+    @PutMapping("/deleteById")
+    public void update(@PathVariable("id") long id){
+        restTemplate.put("http://localhost:8010/student/save",id);
+    }
+
+}
