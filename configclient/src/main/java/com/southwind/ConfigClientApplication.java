a1627b7e32092ecbd0ca91a5857fc4faafe4890b
luzacao <1565081157@qq.com>
2020-08-01 15:46:12 +0800

    qwe

--- configclient/src/main/java/com/southwind/ConfigClientApplication.java
+++ configclient/src/main/java/com/southwind/ConfigClientApplication.java
@@ -3,6 +3,9 @@ package com.southwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
--- configclient/src/main/java/com/southwind/controller/HelloHandler.java
+++ configclient/src/main/java/com/southwind/controller/HelloHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Administrator
+ */
 @RestController
 @RequestMapping("/hello")
 public class HelloHandler {
