aad9766e88a0fadb33487da1866e615b45ec3241
luzacao <1565081157@qq.com>
2020-08-01 15:52:31 +0800

    Initial commit

--- configclient/src/main/java/com/southwind/controller/HelloHandler.java
+++ configclient/src/main/java/com/southwind/controller/HelloHandler.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Administrator
- */
+
 @RestController
 @RequestMapping("/hello")
 public class HelloHandler {
